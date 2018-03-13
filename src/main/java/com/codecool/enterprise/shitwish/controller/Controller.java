3655d6276b0089375d23b0813952eddf010dedda
Kristóf <illeskristof1994@gmail.com>
2018-03-13 13:16:14 +0100

    index

--- src/main/java/com/codecool/enterprise/shitwish/controller/Controller.java
+++ src/main/java/com/codecool/enterprise/shitwish/controller/Controller.java
@@ -3,6 +3,7 @@ package com.codecool.enterprise.shitwish.controller;
 import com.codecool.enterprise.shitwish.service.ApiService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -14,11 +15,19 @@ public class Controller {
     private ApiService apiService;
 
     private String EXAMPLEAPIURL = "https://api.chucknorris.io/jokes/random";
+    private String PRODUCTSAPIURL = "productsapi.com"
 
     @GetMapping(value = "/getjson")
     public String getJson() throws IOException {
         JSONObject jsonInfo = apiService.getJson(EXAMPLEAPIURL);
         return jsonInfo.toMap().toString();
+    }
+
+    @GetMapping(value = "/")
+    public String landingPage(Model model) throws IOException {
+        model.addAttribute("products", apiService.getJson(PRODUCTSAPIURL).toMap().toString());
+
+        return "/index";
     }
 
 }
