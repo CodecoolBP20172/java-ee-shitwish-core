ddd15d562d741a468e49b17ef979039340e51821
sevendiko <sevendiko@gmail.com>
2018-03-22 17:05:00 +0100

    WIP product page

--- src/main/java/com/codecool/enterprise/shitwish/controller/ViewController.java
+++ src/main/java/com/codecool/enterprise/shitwish/controller/ViewController.java
@@ -1,6 +1,7 @@
 package com.codecool.enterprise.shitwish.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,4 +15,10 @@ public class ViewController {
 
     @RequestMapping(value = "/shopping_cart", method = RequestMethod.GET)
     public String renderCart() { return "cart";}
+
+    @RequestMapping(value = "/product/{id}", method = RequestMethod.GET)
+    public String renderCart(@PathVariable long id) {
+        return "product";
+    }
+
 }
