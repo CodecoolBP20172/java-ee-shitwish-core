fcc7836724b4bc85ea9071fce19520cf2774bd59
Attila Racz <racz.attila89@gmail.com>
2018-03-27 13:56:10 +0200

    main controller shopping cart, product routes created

--- src/main/java/com/codecool/enterprise/shitwish/controller/MainController.java
+++ src/main/java/com/codecool/enterprise/shitwish/controller/MainController.java
@@ -33,14 +33,8 @@ public class MainController {
             "{\"size\" : 20, \"totalElements\" : 2, \"totalPages\" : 1, \"number\" : 0}}";
 
     //for test purpose only
-    String testProductString = "{\"_embedded\" : {\"product\" : [\"listOfTags\" : [ ], \"name\" : \"Prod1\", \"price\" : 10.0, +" +
-            "\"description\" : \"Hablaty\", \"active\" : \"true\", \"_links\" : {\"self\" : {\"href\" : \"http://localhost:60001/products/1\"}" +
-            "\"product\" : {\"href\" : \"http://localhost:60001/products/1\"}}},{\"listOfTags\" : [ ],\"name\" : \"Prod2\", \"price\" : 200.0," +
-            "\"description\" : \"Akarmi\", \"active\" : \"true\", \"_links\" : {\"self\" : {\"href\" : \"http://localhost:60001/products/2\"}," +
-            "\"product\" : {\"href\" : \"http://localhost:60001/products/2\"}}} ]}, \"_links\" : { \"self\" : { \"href\" : " +
-            "\"http://localhost:60001/products{?page,size,sort}\", \"templated\" : true}, \"profile\" : {\"href\" : " +
-            "\"http://localhost:60001/profile/products\"}, \"search\" : {\"href\" : \"http://localhost:60001/products/search\"}},\"page\" : " +
-            "{\"size\" : 20, \"totalElements\" : 2, \"totalPages\" : 1, \"number\" : 0}}";
+    String testProductString = "{listOfTags : [ ], name : \"Prod1\", price : 10.0, description : \"Hablaty\", status : \"ACTIVE\", _links : " +
+            "{self : {href : \"http://localhost:60001/products/1\"}, product : {href : \"http://localhost:60001/products/1\"}}}";
 
     @Autowired
     private ApiService apiService;
@@ -59,7 +53,6 @@ public class MainController {
         return "/index";
     }
 
-    //todo
     @RequestMapping(value = "/shopping_cart", method = RequestMethod.GET)
     public String renderCart(Model model) {
         //commented out till products ms is up on heroku:
@@ -67,11 +60,10 @@ public class MainController {
         model.addAttribute("products", testCartProductsString); //till products ms not available
         return "cart";}
 
-    //todo
     @RequestMapping(value = "/product/{id}", method = RequestMethod.GET)
     public String renderCart(@PathVariable long id, Model model) {
         //commented out till products ms is up on heroku:
-        //model.addAttribute("products", apiService.getJson(PRODUCTAPIURL).toMap().toString());
+        //model.addAttribute("product", apiService.getJson(PRODUCTAPIURL).toMap().toString());
         model.addAttribute("product", testProductString); //till products ms not available
         return "product";
     }
